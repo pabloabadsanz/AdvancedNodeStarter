d0ee93a8ecb279f5406896cc9040a596f92b81b5
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-28 08:19:50 +0200

    First chromium launch a browser test, checking innerHTML

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,4 +11,9 @@ test('We can launch a browser', async () => {
     headless: false
   });
   const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 });
