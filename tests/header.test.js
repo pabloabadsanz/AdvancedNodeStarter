b36e2992a9005d799e80740a2291cc58ad5132c8
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-28 09:01:13 +0200

    Mocking Oauth sessions in test

--- tests/header.test.js
+++ tests/header.test.js
@@ -33,3 +33,31 @@ test('clicking login starts oauth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test.only('When signed in, shows logout button', async () => {
+  const id = '5ba9dad4f4ecc1129b0723c8';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(
+    JSON.stringify(sessionObject)
+  ).toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
+});
