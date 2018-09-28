3cc537ad1958e194c713519b8189832b89068f3e
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-28 08:34:08 +0200

    Added test which checks URL after clicking OAUTH link

--- tests/header.test.js
+++ tests/header.test.js
@@ -20,8 +20,16 @@ test('Adds two numbers', () => {
   expect(sum).toEqual(3);
 });
 
-test('We can launch a browser', async () => {
+test('the header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
